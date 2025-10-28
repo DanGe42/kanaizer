87625a162e4ecdc47f3ceb490253a3cd30fd873a
Daniel Ge <DanGe42@users.noreply.github.com>
2025-10-28 00:26:49 -0700

    Support different kana modes in CLI

--- src/main/kotlin/io/dge/kanaizer/Main.kt
+++ src/main/kotlin/io/dge/kanaizer/Main.kt
@@ -2,27 +2,48 @@ package io.dge.kanaizer
 
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.core.main
+import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.flag
 import com.github.ajalt.clikt.parameters.options.help
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.types.choice
 import io.dge.kanaizer.translator.Translator
 import io.dge.kanaizer.tree.HIRAGANA_MAPPINGS
+import io.dge.kanaizer.tree.KATAKANA_MAPPINGS
 
 class Kanaizer : CliktCommand() {
     val applyPimsleurFilter: Boolean by option("--pimsleur")
         .flag()
         .help("Apply Pimsleur filter. Useful for romaji coming from Pimsleur flashcards.")
 
+    val kanaMode: String? by option("-m", "--mode")
+        .choice("hiragana", "katakana", "mixed")
+        .default("hiragana")
+        .help("Kana mode. Indicates what type of input it will be.")
+
+    val showAmbiguity: Boolean by option("-a", "--show-ambiguity")
+        .flag()
+        .help("Show ambiguity if a romaji sequence can result in multiple characters.")
+
     override fun run() {
         echo("Pimsleur filter = $applyPimsleurFilter")
 
-        val translator = Translator(HIRAGANA_MAPPINGS)
+        val translator = buildTranslator()
 
         var input: String? = readlnOrNull()
         while (input != null) {
             val translated = translator.translate(input)
             echo(translated)
             input = readlnOrNull()
+        }
+    }
+
+    private fun buildTranslator(): Translator {
+        return when (kanaMode) {
+            "hiragana" -> Translator(HIRAGANA_MAPPINGS, showAmbiguity = showAmbiguity)
+            "katakana" -> Translator(KATAKANA_MAPPINGS, showAmbiguity = showAmbiguity)
+            "mixed" -> TODO("Not yet implemented")
+            else -> throw IllegalArgumentException("Invalid mode `$kanaMode`")
         }
     }
 }
